34e1ba87572df271701a225c9440958fd08694d9
Felipe Toshio <felipetoshio1214@gmail.com>
2020-09-03 18:30:16 -0300

    Add endpoint for status update

--- functions/index.js
+++ functions/index.js
@@ -25,7 +25,16 @@ admin.initializeApp({
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 app.get('/addDatabase', (req,res) => {
-    res.send('server running');
+    var reference = admin.database().ref("database");
+
+    reference.on("queue", function(data){
+        res.send(data.val());
+    },
+    function (error){
+        res.send("Failed");
+    });
+
+    res.send(response);
 });
 
 app.post('/addDatabase', jsonParser, async (req,res) => {
@@ -33,18 +42,36 @@ app.post('/addDatabase', jsonParser, async (req,res) => {
     var db = admin.database();
     var ref = db.ref("database");
 
-    var name = req.body.name
+    var uniqueId = req.body.uniqueId
 
-    var queueRef = ref.child("queue/" + name);
+    var queueRef = ref.child("queue/" + uniqueId);
+
+    let date_ob = new Date();
+    let date = ("0" + date_ob.getDate()).slice(-2);
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    let year = date_ob.getFullYear();
+    let hours = date_ob.getHours() - 3;
+    let minutes = date_ob.getMinutes();
+    let seconds = date_ob.getSeconds();
+
+    let ts = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
 
     queueRef.set({
+        name: req.body.name,
         latitude: req.body.latitude,
         longitude: req.body.longitude,
-        timestamp: req.body.timestamp
-        
+        timestamp: ts,
+        status: false
     });
 
     res.send("created");
 });
+
+app.post("/updateStatus", jsonParser, async(req,res) =>{
+
+    var dbRef = admin.database().ref("database/queue/" + req.body.uniqueId).update({status: true});
+
+    res.send("updated")
+})
 
 exports.addCoordinates = functions.https.onRequest(app);
